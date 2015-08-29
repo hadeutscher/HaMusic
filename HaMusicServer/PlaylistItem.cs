64200ecda4178746deceaa09c0972eb4e93494cc
haha01haha01 <haha01haha01@gmail.com>
2015-08-29 17:01:41 +0300

    Basic func

---
+++ HaMusicServer/PlaylistItem.cs
@@ -0,0 +1,44 @@
+﻿using System.Threading;
+
+namespace HaMusicServer
+{
+    public class PlaylistItem
+    {
+        private static long nextUid = 0;
+
+        private long uid;
+        private string item;
+
+        public PlaylistItem(string item)
+        {
+            this.uid = Interlocked.Increment(ref nextUid);
+            this.item = item;
+        }
+
+        public long UID
+        {
+            get
+            {
+                return uid;
+            }
+
+            set
+            {
+                uid = value;
+            }
+        }
+
+        public string Item
+        {
+            get
+            {
+                return item;
+            }
+
+            set
+            {
+                item = value;
+            }
+        }
+    }
+}
