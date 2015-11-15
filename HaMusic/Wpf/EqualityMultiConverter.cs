c4c4ca86cf0ef77895ae7dfad20410752c8a58d1
haha01haha01 <haha01haha01@gmail.com>
2015-11-15 20:23:00 +0200

    Next overried as if ended is invisible

--- HaMusic/Wpf/EqualityMultiConverter.cs
+++ HaMusic/Wpf/EqualityMultiConverter.cs
@@ -4,7 +4,9 @@
 * License, v. 2.0. If a copy of the MPL was not distributed with this
 * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+using HaMusicLib;
 using System;
+using System.Collections.Generic;
 using System.Windows.Data;
 
 namespace HaMusic.Wpf
@@ -14,7 +16,34 @@ namespace HaMusic.Wpf
         public object Convert(object[] values, Type targetType, object parameter,
             System.Globalization.CultureInfo culture)
         {
-            return values.Length == 2 && values[0] is long && values[1] is long && (long)values[0] == (long)values[1];
+            bool first_long_set = false;
+            long curr_long = 0;
+            foreach (object val in values)
+            {
+                if (val is long)
+                {
+                    if (first_long_set)
+                    {
+                        if ((long)val != curr_long)
+                            return false;
+                    }
+                    else
+                    {
+                        curr_long = (long)val;
+                        first_long_set = true;
+                    }
+                }
+                else if (val is HaProtoImpl.InjectionType)
+                {
+                    if ((HaProtoImpl.InjectionType)val == HaProtoImpl.InjectionType.INJECT_AS_IF_SONG_ENDED)
+                        return false;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return true;
         }
 
         public object[] ConvertBack(object value, Type[] targetTypes, object parameter,
